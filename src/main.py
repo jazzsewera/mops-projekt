eb5eb6b168caff95792e65e7d0146301330e5d3c
batmatt <m.w.1999@o2.pl>
2021-01-03 18:06:32 +0100

    #8 Create PacketGenarator and GeneratorParameters classes

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 import logging as log
 from simulator.rand import Rand
 from simulator.timer import Timer
+from simulator.state import SimulationTime
+from simulator.state import GeneratorParameters
 
 
 def on_state_handler():
@@ -10,11 +12,18 @@ def on_state_handler():
 def off_state_handler():
     log.debug("Off event fired")
 
+def set_generator_parameters():
+    GeneratorParameters.set_packet_length(input("Enter packet length: "))
+    GeneratorParameters.set_generation_time(input("Enter generation time: "))
+    GeneratorParameters.set_streams_number(input("Enter a number of streams: "))
 
 def main():
     log.getLogger().setLevel(log.DEBUG)
-
     log.debug("Program started")
+
+    set_generator_parameters()
+    GeneratorParameters.get_packet_lenght()
+
     simulation_time = 5
     rand = Rand(0.5, 0.5)
     timer = Timer(rand, simulation_time)
--- src/simulator/state.py
+++ src/simulator/state.py
@@ -1,18 +1,49 @@
-class State(object):
-    class SimulationTime(object):
-        _simulation_time = 0
+class SimulationTime(object):
+    _simulation_time = 0
 
-        def __init__(self):
-            raise RuntimeError("Call get() instead")
+    def __init__(self):
+        raise RuntimeError("Call get() instead")
 
-        @classmethod
-        def get(cls):
-            return cls._simulation_time
+    @classmethod
+    def get(cls):
+        return cls._simulation_time
 
-        @classmethod
-        def set(cls, time):
-            cls._simulation_time = time
+    @classmethod
+    def set(cls, time):
+        cls._simulation_time = time
 
-        @classmethod
-        def increment(cls):
-            cls._simulation_time += 1
+    @classmethod
+    def increment(cls):
+        cls._simulation_time += 1
+
+class GeneratorParameters(object):
+    _packet_length = 0
+    _generation_time = 0
+    _streams_number = 0
+    
+    def __init__(self):
+        raise RuntimeError("Call get() instead")
+
+    @classmethod
+    def get_packet_lenght(cls):
+        return cls._packet_length
+
+    @classmethod
+    def set_packet_length(cls, packet_length):
+        cls._packet_length = packet_length
+
+    @classmethod
+    def get_generation_time(cls):
+        return cls._generation_time
+
+    @classmethod
+    def set_generation_time(cls, generation_time):
+        cls._generation_time = generation_time
+
+    @classmethod
+    def get_streams_number(cls):
+        return cls._streams_number
+
+    @classmethod
+    def set_streams_number(cls, streams_number):
+        cls._streams_number = streams_number
