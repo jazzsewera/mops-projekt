b6172be7ed4e0550aedbd8c8a440eac32566c019
kovalskj <wodygo@gmail.com>
2021-01-05 11:03:32 +0100

    added second queue

--- src/simulator/queue.py
+++ src/simulator/queue.py
@@ -22,6 +22,7 @@ class Queue(object):
 
     def queue_packet_receiver(self, packet: Packet):
         self.packets.append(packet)
+        log.info("Queue recived new packet")
 
     def queue_packet_listener(self, current_time):
         self.packets_number[current_time] = len(self.packets)
@@ -31,16 +32,26 @@ class Queue(object):
             self._current_packet = self.packets.pop(0)
             self._current_packet_remaining_time = self._service_time - 1
             self._service_time_start = self._current_time
-            self._current_packet.out_of_queue_time = self._service_time_start
+            if self._queue is not None:
+                self._current_packet.out_of_queue_time = self._service_time_start
+            #else:
+            #    self._current_packet.out_of_second_queue_time = self._service_time_start
         elif self._current_packet and self._current_packet_remaining_time:
             self._current_packet_remaining_time -= 1
             log.debug(f"Server busy, currently handling packet: {self._current_packet}")
         elif self._current_packet and not self._current_packet_remaining_time:
-            self._current_packet.in_second_queue_time = self._current_time
+            if self._queue is not None:
+                self._current_packet.in_second_queue_time = self._current_time
+                log.info("###Q1###")
+            else:
+                self._current_packet.out_of_second_queue = self._current_time
+                log.info("###Q2###")
             log.info(f"Finished handling packet: {self._current_packet}")
-                # TODO: decide if the packet should be sent
             if self._current_packet.is_passing:
                 self.packets_passed.append(self._current_packet)
+                if self._queue is not None:
+                    self._queue.queue_packet_receiver(self._current_packet)
+                    log.info("Sending packet to next queue")
             #self._queue.queue_packet_receiver(self._current_packet)
             self._current_packet = None
         else:
