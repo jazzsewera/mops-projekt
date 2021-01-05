9c5f6b3d097a7d88cc248e63f43f2ee64f71c2a7
Blazej Sewera <code@sewera.dev>
2021-01-05 14:50:32 +0100

    Change generators to be event-based

--- src/main.py
+++ src/main.py
@@ -1,17 +1,21 @@
 import logging as log
+from typing import List
 
+from data_reader import *
+from simulator.event_queue import EventQueue
 from simulator.packet_generator import PacketGenerator
 from simulator.queue import Queue
 from simulator.state import GeneratorParameters
 from simulator.timer import Timer
-from data_reader import *
 
 
 def set_generator_parameters():
     GeneratorParameters.set_packet_length(input("Enter packet length: "))
     GeneratorParameters.set_generation_time(input("Enter generation time: "))
     GeneratorParameters.set_streams_number(input("Enter a number of streams: "))
-    GeneratorParameters.set_dropped_streams(input("Enter number of streams dropped after leaving first queue:")) # TODO assertion that dropped streams < all streams
+    GeneratorParameters.set_dropped_streams(
+        input("Enter number of streams dropped after leaving first queue:")
+    )  # TODO assertion that dropped streams < all streams
 
 
 def main():
@@ -22,56 +26,74 @@ def main():
 
     simulation_time = 50
     timer = Timer(simulation_time)
+    event_queue = EventQueue(timer)
     queue_two = Queue(timer, GeneratorParameters.get_packet_length())
     queue_one = Queue(timer, GeneratorParameters.get_packet_length(), queue_two)
-    timer.add_clock_event_listener(queue_one.queue_packet_listener)
-    timer.add_clock_event_listener(queue_two.queue_packet_listener)
 
-    generator_pool = []
+    generator_pool: List[PacketGenerator] = []
 
     dropped_number = GeneratorParameters.get_dropped_streams()
 
     for _ in range(GeneratorParameters.get_streams_number()):
         if GeneratorParameters.get_dropped_streams() > 0:
             generator = PacketGenerator(
-                timer, queue_one, GeneratorParameters.get_packet_length(), 1, False
+                timer,
+                event_queue,
+                queue_one,
+                GeneratorParameters.get_packet_length(),
+                1,
+                False,
             )
-            timer.add_clock_event_listener(generator.generator_event_listener)
             generator_pool.append(generator)
-            GeneratorParameters.set_dropped_streams(GeneratorParameters.get_dropped_streams() - 1)
+            GeneratorParameters.set_dropped_streams(
+                GeneratorParameters.get_dropped_streams() - 1
+            )
         else:
             generator = PacketGenerator(
-                timer, queue_one, GeneratorParameters.get_packet_length(), 1, True
+                timer,
+                event_queue,
+                queue_one,
+                GeneratorParameters.get_packet_length(),
+                1,
+                True,
             )
-            timer.add_clock_event_listener(generator.generator_event_listener)
             generator_pool.append(generator)
 
     for _ in range(dropped_number):
         generator = PacketGenerator(
-            timer, queue_two, GeneratorParameters.get_packet_length(), 1, True
+            timer,
+            event_queue,
+            queue_two,
+            GeneratorParameters.get_packet_length(),
+            1,
+            True,
         )
-        timer.add_clock_event_listener(generator.generator_event_listener)
         generator_pool.append(generator)
 
-    timer.start_timer_event_loop()
+    for generator in generator_pool:
+        generator.generate_packet()
 
-    print("queue one data:")
-    print(queue_one.packets_number)
-    print(queue_one.packets)
-    print(queue_one.packets_passed)
+    while event_queue.handle_event():
+        pass
+
+    log.debug("queue one data:")
+    log.debug(queue_one.packets_number)
+    log.debug(queue_one.packets)
+    log.debug(queue_one.packets_passed)
     show_queue_length_average(queue_one.packets_number)
     show_average_queue_waiting_time_Q1(queue_one.packets_passed)
     show_average_delay_Q1(queue_one.packets_passed)
     show_average_server_load_Q1(queue_one.packets_passed)
 
-    print("queue two data:")
-    print(queue_two.packets_number)
-    print(queue_two.packets)
-    print(queue_two.packets_passed)
+    log.debug("queue two data:")
+    log.debug(queue_two.packets_number)
+    log.debug(queue_two.packets)
+    log.debug(queue_two.packets_passed)
     show_queue_length_average(queue_two.packets_number)
     show_average_queue_waiting_time_Q2(queue_two.packets_passed)
     show_average_delay_Q2(queue_two.packets_passed)
     show_average_server_load_Q2(queue_two.packets_passed)
+
 
 if __name__ == "__main__":
     main()
