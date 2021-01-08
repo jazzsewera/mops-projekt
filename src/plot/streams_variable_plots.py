bf47d43617a3840f44ff380feccc5964409fa584
Blazej Sewera <code@sewera.dev>
2021-01-08 20:28:41 +0100

    Add streams variable plots

--- src/plot/streams_variable_plots.py
+++ src/plot/streams_variable_plots.py
@@ -5,6 +5,9 @@ import matplotlib.pyplot as plt
 
 parser = argparse.ArgumentParser()
 parser.add_argument("filename", help="Input filename")
+parser.add_argument(
+    "-o", "--outfolder", help="Output (plot) folder", type=str, required=True
+)
 args = parser.parse_args()
 
 data = []
@@ -15,41 +18,34 @@ with open(args.filename, "r") as in_file:
 
 names = {
     "avg_queue_length_Q1": "Średnia liczba pakietów w kolejce pierwszej",
-    "avg_queue_waiting_time_Q1": "Średni czas oczekiwania w kolejce pierwszej",
-    "avg_delay_Q1": "Średnie opóźnienie przekazu pakietu w kolejce pierwszej",
+    "avg_queue_waiting_time_Q1": "Średni czas oczekiwania w kolejce pierwszej [s]",
+    "avg_delay_Q1": "Średnie opóźnienie przekazu pakietu w kolejce pierwszej [s]",
     "avg_load_Q1": "Średnie obciążenie serwera pierwszego",
     "packets_passed_Q1": "Pakiety obsłużone przez pierwszy węzeł",
     "avg_queue_length_Q2": " Średnia liczba pakietów w kolejce drugiej",
-    "avg_queue_waiting_time_Q2": "Średni czas oczekiwania w kolejce drugiej",
-    "avg_delay_Q2": "Średnie opóźnienie przekazu pakietu w kolejce drugiej",
+    "avg_queue_waiting_time_Q2": "Średni czas oczekiwania w kolejce drugiej [s]",
+    "avg_delay_Q2": "Średnie opóźnienie przekazu pakietu w kolejce drugiej [s]",
     "avg_load_Q2": "Średnie obciążenie serwera drugiego",
     "packets_passed_Q2": "Pakiety obsłużone przez drugi węzeł",
 }
 
+for k, _ in names.items():
+    legend = []
+    for i in range(5):
+        xs = [
+            d["simulation_params"]["streams_number"]
+            for d in data
+            if d["simulation_params"]["dropped_streams"] == i
+        ]
 
-xs_0 = [
-    d["simulation_params"]["streams_number"]
-    for d in data
-    if d["simulation_params"]["dropped_streams"] == 0
-]
-xs_1 = [
-    d["simulation_params"]["streams_number"]
-    for d in data
-    if d["simulation_params"]["dropped_streams"] == 1
-]
+        ys = [d[k] for d in data if d["simulation_params"]["dropped_streams"] == i]
 
-ys_0 = [
-    d["avg_queue_length_Q1"]
-    for d in data
-    if d["simulation_params"]["dropped_streams"] == 0
-]
-ys_1 = [
-    d["avg_queue_length_Q1"]
-    for d in data
-    if d["simulation_params"]["dropped_streams"] == 1
-]
-
-plt.plot(xs_0, ys_0)
-plt.plot(xs_1, ys_1)
-plt.title(names["avg_queue_length_Q1"])
-plt.show()
+        plt.plot(xs, ys)
+        legend.append(
+            f"Liczba strumieni porzuconych po 1. węźle = {i}",
+        )
+    plt.title(names[k])
+    plt.xlabel("Liczba strumieni wpływających")
+    plt.legend(legend)
+    plt.savefig(f"{args.outfolder}/{k}.png")
+    plt.clf()
