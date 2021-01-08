ddb2809ad8e5062ec5bb48682c13bf4abe73330b
batmatt <m.w.1999@o2.pl>
2021-01-08 19:47:55 +0100

    Plot

--- src/plot/main.py
+++ src/plot/main.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import argparse
 import json
 
@@ -11,4 +12,31 @@ with open(args.filename, "r") as in_file:
     for line in in_file.readlines():
         data.append(json.loads(line))
 
-print(data)
+names = {'avg_queue_length_Q1': 'Średnia liczba pakietów w kolejce pierwszej',
+           'avg_queue_waiting_time_Q1': 'Średni czas oczekiwania w kolejce pierwszej',
+           'avg_delay_Q1': 'Średnie opóźnienie przekazu pakietu w kolejce pierwszej',
+           'avg_load_Q1': 'Średnie obciążenie serwera pierwszego',
+           'packets_passed_Q1': 'Pakiety obsłużone przez pierwszy węzeł',
+           'avg_queue_length_Q2':' Średnia liczba pakietów w kolejce drugiej',
+           'avg_queue_waiting_time_Q2': 'Średni czas oczekiwania w kolejce drugiej',
+           'avg_delay_Q2': 'Średnie opóźnienie przekazu pakietu w kolejce drugiej',
+           'avg_load_Q2': 'Średnie obciążenie serwera drugiego',
+           'packets_passed_Q2': 'Pakiety obsłużone przez drugi węzeł'}
+
+i = 0
+
+for key, value in data[0].items():
+  val = []
+  if key == "simulation_params":
+    continue
+  print(key)
+  tail_data = data[-10:]
+  for measure in tail_data:
+    val.append(measure[key])
+    print(measure)
+  plt.figure(i)
+  plt.plot(range(len(val)), val)
+  plt.title(names[key])
+  plt.ylim([0, max(val)*1.1])
+  #print(val)
+  i += 1
